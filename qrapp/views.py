f317faf2133a48e462297d2a2024526145f00802
Abdul Wasay <abdulwasayrao917@gmail.com>
2026-05-13 12:32:02 +0500

    UI update

--- qrapp/views.py
+++ qrapp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+import qrcode
+import base64
+from io import BytesIO
+
 
 def index(request):
-    return render(request, 'qrapp/index.html')
+
+    qr = None
+
+    if request.method == "POST":
+
+        text = request.POST.get("qr_text")
+
+        qr_image = qrcode.make(text)
+
+        buffer = BytesIO()
+        qr_image.save(buffer, format='PNG')
+
+        qr_base64 = base64.b64encode(buffer.getvalue()).decode()
+
+        qr = f"data:image/png;base64,{qr_base64}"
+
+    return render(request, 'qrapp/index.html', {'qr': qr})
